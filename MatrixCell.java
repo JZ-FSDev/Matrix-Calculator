5d9550b351c7d752058c39e3f5fde25e539f5f4a
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-02-10 17:04:42 -0600

    Update MatrixCell.java

--- MatrixCell.java
+++ MatrixCell.java
@@ -42,5 +42,30 @@ public class MatrixCell {
         numerator += otherNumerator;
 
         simplify();
+    }
+    
+    public void simplify(){
+        numerator /= gcd(numerator, denominator);
+        denominator /= gcd(numerator, denominator);
+    } 
+
+    public int gcd(int a, int b){
+        int gcd = 1;
+        for(int i = 1; i <= Math.min(a, b); i++){
+            if(a % i == 0 && b % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    public int lcd(int a, int b){
+        int lcd = 0;
+        for(int i = Math.max(a,b); i <= a*b && lcd == 0; i++){
+            if(i % a == 0 && i % b == 0){
+                lcd = i;
+            }   
+        }
+        return lcd;
     }    
 }

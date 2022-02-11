6454c21013e62d85f300ae4bd75b022dd8a0d67d
Jason Zhu <94236556+JZ-FSDev@users.noreply.github.com>
2022-02-11 16:03:54 -0600

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,22 @@
+public class Matrix {
+    
+    private MatrixCell[][] matrix;
+
+    public Matrix(int width, int length, String[] nums){
+        matrix = new MatrixCell[width][length];
+        int count = 0;
+        Scanner scan;
+        String[] temp;
+        for(int i = 0; i < matrix.length; i++){
+            for(int k = 0; k < matrix[i].length; k++){
+                scan = new Scanner(nums[count]);
+                temp = nums[count++].split("/");
+                if(temp.length == 2){
+                    matrix[i][k] = new MatrixCell(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
+                }else{
+                    matrix[i][k] = new MatrixCell(Integer.parseInt(temp[0]), 1);
+                }
+            }
+        } 
+    }
+}
